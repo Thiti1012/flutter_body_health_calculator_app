50ab21fec31e20c1ce0825ea83914eeaa61f8033
Thiti1012 <s6619410031@sau.ac.th>
2026-01-29 01:54:15 +0700

    Commit03: ทำ Navigationbar ที่ Home Ui เสร็จเเล้ว

--- lib/views/home_ui.dart
+++ lib/views/home_ui.dart
@@ -1,4 +1,8 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/about_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmi_ui.dart';
+import 'package:flutter_body_health_calculator_app/views/bmr_ui.dart';
+import 'package:font_awesome_flutter/font_awesome_flutter.dart';
 
 class HomeUi extends StatefulWidget {
   const HomeUi({super.key});
@@ -8,8 +12,55 @@ class HomeUi extends StatefulWidget {
 }
 
 class _HomeUiState extends State<HomeUi> {
+  //สร้างตัวแปรควบคุม currentIndex ของ BarItem
+  int currentIndexStatus = 1;
+  List<Widget> showBody = [BmiUi(), AboutUi(), BmrUi()];
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.green,
+        title: Text(
+          'Body Health Calculator',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: MediaQuery.of(context).size.height * 0.025,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        centerTitle: true,
+      ),
+      bottomNavigationBar: BottomNavigationBar(
+        onTap: (value) {
+          setState(() {
+            currentIndexStatus = value;
+          });
+        },
+        currentIndex: currentIndexStatus,
+        selectedItemColor: Colors.green,
+        items: [
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.person_3,
+            ),
+            label: 'BMI',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              Icons.home,
+            ),
+            label: 'About',
+          ),
+          BottomNavigationBarItem(
+            icon: Icon(
+              FontAwesomeIcons.personBooth,
+            ),
+            label: 'BMR',
+          ),
+        ],
+      ),
+      body: showBody[currentIndexStatus],
+    );
   }
 }
--- lib/views/splash_screen_ui.dart
+++ lib/views/splash_screen_ui.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_body_health_calculator_app/views/home_ui.dart';
 
 class SplashScreenUi extends StatefulWidget {
   const SplashScreenUi({super.key});
@@ -9,9 +10,54 @@ class SplashScreenUi extends StatefulWidget {
 
 class _SplashScreenUiState extends State<SplashScreenUi> {
   @override
+  void initState() {
+    // ให้หน้าจอสแปลชแสดงผลเป็นเวลา 3 วินาที แล้วเปลี่ยนไปยังหน้า HomeUi
+    Future.delayed(
+      //เวลาที่หน่วง
+      Duration(seconds: 3),
+      //เมื่อครบเวลาให้เปลี่ยนหน้า
+      () {
+        Navigator.pushReplacement(
+          context,
+          MaterialPageRoute(
+            builder: (context) => HomeUi(),
+          ),
+        );
+      },
+    );
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Color(0xFFE7431B),
+      backgroundColor: Colors.green,
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Image.asset(
+              'assets/images/calculator.png',
+              width: MediaQuery.of(context).size.width * 0.5,
+              height: MediaQuery.of(context).size.width * 0.5,
+              fit: BoxFit.cover,
+            ),
+            SizedBox(height: 20),
+            Text(
+              'Body Health Calculator',
+              style: TextStyle(
+                fontSize: MediaQuery.of(context).size.height * 0.025,
+                color: Colors.white,
+                fontWeight: FontWeight.bold,
+              ),
+            ),
+            SizedBox(height: 20),
+            CircularProgressIndicator(
+              color: Colors.white,
+            ),
+          ],
+        ),
+      ),
     );
   }
 }
